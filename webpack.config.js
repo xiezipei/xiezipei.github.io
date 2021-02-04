1d0b2b0b3639a18f9a543b986ddbd15f4a352dc9
xiezipei <xiezipei@zckd>
2021-02-04 14:40:55 +0800

    update(webpack): 回退处理，准备学习 `管理输出`

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,4 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  module: {     // 模块
-      rules: [
-          {
-              test: /\.css$/,   // 处理css文件
-              use: [
-                  'style-loader',
-                  'css-loader',
-              ],
-          },
-          {
-              test: /\.(png|svg|jpg|gif)$/, // 处理图片
-              use: [
-                  'file-loader',
-              ],
-          },
-          {
-              test: /\.(woff|woff2|eot|ttf|otf)$/,  // 处理字体
-              use: [
-                'file-loader',
-              ]
-          },
-      ],
-  },
 };
