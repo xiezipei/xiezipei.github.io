2e06e840965686c1d28a411fd35a6400b88377c9
xiezipei <xiezipei@zckd>
2021-02-04 14:57:52 +0800

    update(webpack): 使用相关plugin清理 `dist` 目录&动态生成 `index.html` 文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: {
     app: './src/index.js',
     print: './src/pirnt.js',
   },
-  output: {     // 输出
+  plugins: [
+    new CleanWebpackPlugin(),   // 清理`/dist`文件夹
+    new HtmlWebpackPlugin({     // 动态生成`index.html`文件
+        title: '管理输出',
+    })
+  ],
+  output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
