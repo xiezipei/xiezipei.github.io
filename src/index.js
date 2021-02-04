df809bd83db6a46768a68ff106164039b62e995c
xiezipei <xiezipei@zckd>
2021-02-04 14:38:53 +0800

    update: 使用 loader 处理其他资源文件

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import _ from 'lodash';
+import './style.css';
+import bg from './bg.jpg';
 
 function component() {
     const element = document.createElement('div');
+
+    // 创建dom元素
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    // 添加class类名
+    element.classList.add('hello');
+
+    // 添加图片
+    const bgImage = new Image();
+    bgImage.src = bg;
+    element.appendChild(bgImage);
+
     return element;
   }
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
+  entry: './src/index.js',  // 入口
+  output: {     // 输出
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {     // 模块
+      rules: [
+          {
+              test: /\.css$/,   // 处理css文件
+              use: [
+                  'style-loader',
+                  'css-loader',
+              ],
+          },
+          {
+              test: /\.(png|svg|jpg|gif)$/, // 处理图片
+              use: [
+                  'file-loader',
+              ],
+          },
+          {
+              test: /\.(woff|woff2|eot|ttf|otf)$/,  // 处理字体
+              use: [
+                'file-loader',
+              ]
+          },
+      ],
   },
 };
