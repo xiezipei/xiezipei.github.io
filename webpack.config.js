85851ac3e4ea913ef0d1cc2517ec9d67687f7867
xiezipei <xiezipei@zckd>
2021-02-04 15:02:36 +0800

    update(webpack): 使用开发模式 & 启用 sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -3,18 +3,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-    print: './src/pirnt.js',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),   // 清理`/dist`文件夹
-    new HtmlWebpackPlugin({     // 动态生成`index.html`文件
-        title: '管理输出',
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',    // 开发模式
+    entry: {
+        app: './src/index.js',
+        print: './src/pirnt.js',
+    },
+    devtool: 'inline-source-map',   // 启用sourcemap，请注意不要在生产环境使用！！！
+    plugins: [
+        new CleanWebpackPlugin(),   // 清理`/dist`文件夹
+        new HtmlWebpackPlugin({     // 动态生成`index.html`文件
+            title: '管理输出',
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
