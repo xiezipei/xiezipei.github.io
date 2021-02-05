934c81db20139ef792dc47acb447a1674eecffd0
xiezipei <xiezipei@zckd>
2021-02-05 11:32:08 +0800

    update(webpack): 模块热替换（MHR）

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-// import Print from './print';
+import Print from './print';
 
 async function getComponent() {
 	try {
 		const { default: _ } = await import('lodash');
 		const element = document.createElement('div');
 		element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-		// element.onclick = Print.bind(null, 'Hello webpack!');
+
+		const btn = document.createElement('button');
+		btn.innerHTML = 'Click me and check console!';
+		btn.onclick = Print.bind(null, 'Hello webpack!');
+		element.appendChild(btn);
+
 		return element;
 	} catch (error) {
 		return 'An error occured while loading the component';
@@ -15,3 +20,10 @@ async function getComponent() {
 getComponent().then(component => {
 	document.body.appendChild(component);
 });
+
+if (module.hot) {
+	module.hot.accept('./print.js', function() {
+		console.log('Accepting the update printMe module!');
+		printMe();
+	});
+}
--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,9 @@ module.exports = {
 
     devtool: 'inline-source-map',   // 启用sourcemap，请注意不要在生产环境使用！！！
 
-    // webpack-dev-server 在编译之后不会写入到任何输出文件。而是将 bundle 文件保留在内存中，
-    // 然后将它们 serve 到 server 中，就好像它们是挂载在 server 根路径上的真实文件一样。
-    // 如果你的页面希望在其他不同路径中找到 bundle 文件，则可以通过 dev server 配置中的 publicPath 选项进行修改。
     devServer: {
         contentBase: './dist',  // 将 dist 目录下的文件 serve 到 localhost:8080 下
+        hot: true,
     },
 
     plugins: [
