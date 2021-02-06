776396360b97d272e5e9b84fa52c6e09dd37bd26
xiezipei <xiezipei@zckd>
2021-02-06 11:18:11 +0800

    update(webpack): 集成渐进式网络应用程序（ServiceWorker）

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import _ from 'lodash';
 
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {
+		navigator.serviceWorker.register('/service-worker.js').then(registration => {
+			console.log('SW registered: ', registration);
+		}).catch(registrationError => {
+			console.log('SW registration failed: ', registrationError);
+		});
+	});
+}
+
 function component() {
 	const element = document.createElement('div');
 	const button = document.createElement('button');
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');    // 引入 workbox
 
 module.exports = {
     entry: {
@@ -22,8 +23,15 @@ module.exports = {
         // 对于 CleanWebpackPlugin 的 v2 versions 以下版本，使用 new CleanWebpackPlugin(['dist/*'])
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Production',
+            title: 'Progressive Web Application',
         }),
+        // 使用 workbox 实现 ServicWorker
+        new WorkboxPlugin.GenerateSW({
+            // 这些选项帮助快速启用 ServiceWorkers
+            // 不允许遗留任何“旧的” ServiceWorkers
+            clientsClaim: true,
+            skipWaiting: true,
+        })
     ],
     output: {
         filename: '[name].bundle.js',
